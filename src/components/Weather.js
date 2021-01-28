7d31841c00fb069403d6b298366c12396bd06862
Mattie43 <mattericksen93@gmail.com>
2021-01-28 17:10:11 -0500

    added weather location search

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,20 +2,43 @@ import { useState } from "react";
 import styled from "styled-components";
 
 export default function Weather() {
-  const [zipcode, setZipcode] = useState("");
-  const [weatherInfo, setWeatherInfo] = useState({});
+  const [search, setSearch] = useState("");
+  const [weatherInfo, setWeatherInfo] = useState([]);
 
-  function fetchWeather(query) {
-    fetch(`https://www.metaweather.com/api/location/search/?query=${query}`)
+  // san fran: "https://www.metaweather.com/api/location/2487956/"
+  // nyc: 2459115
+  // london: 44418
+  //
+  const proxyURL = "https://cors-anywhere.herokuapp.com/";
+
+  function fetchWeather() {
+    fetch(
+      proxyURL +
+        `https://www.metaweather.com/api/location/search/?query=${search}`
+    )
       .then((resp) => resp.json())
       .then((data) => setWeatherInfo(data));
+    // .then((data) => console.log(data));
+  }
+
+  function renderLocations() {
+    return weatherInfo.map((location) => (
+      <li key={location.title} onClick={() => console.log(location.woeid)}>
+        {location.title}
+      </li>
+    ));
   }
 
   return (
     <Container>
       <Body>
-        weather test
-        {fetchWeather()}
+        <input
+          type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button onClick={fetchWeather}>Search for Location</button>
+        <ul>{renderLocations()}</ul>
       </Body>
     </Container>
   );
@@ -24,9 +47,25 @@ export default function Weather() {
 const Container = styled.div`
   display: flex;
   width: 100%;
+  align-items: center;
+  justify-content: center;
 `;
 
 const Body = styled.div`
   display: flex;
-  background-color: salmon;
+  flex-direction: column;
+  & input {
+    outline: none;
+    font-size: 25px;
+  }
+  & button {
+    outline: none;
+    font-size: 20px;
+    padding: 10px;
+    margin: 10px;
+  }
+  & ul {
+    list-style: none;
+    font-size: 20px;
+  }
 `;
