78ded4b7ec95f1927d14489eca3bb2796ca752fd
Mattie43 <mattericksen93@gmail.com>
2021-01-29 15:55:37 -0500

    added location search and weekly weather display

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,42 +3,65 @@ import styled from "styled-components";
 
 export default function Weather() {
   const [search, setSearch] = useState("");
-  const [weatherInfo, setWeatherInfo] = useState([]);
+  const [searchInfo, setSearchInfo] = useState([]);
+  const [weatherInfo, setWeatherInfo] = useState({});
 
   // san fran: "https://www.metaweather.com/api/location/2487956/"
   // nyc: 2459115
   // london: 44418
-  //
+
   const proxyURL = "https://cors-anywhere.herokuapp.com/";
 
-  function fetchWeather() {
+  function fetchSearch() {
     fetch(
       proxyURL +
         `https://www.metaweather.com/api/location/search/?query=${search}`
     )
       .then((resp) => resp.json())
+      .then((data) => setSearchInfo(data));
+    // .then((data) => console.log(data));
+  }
+
+  function fetchWeather(woeid) {
+    fetch(proxyURL + `https://www.metaweather.com/api/location/${woeid}/`)
+      .then((resp) => resp.json())
       .then((data) => setWeatherInfo(data));
     // .then((data) => console.log(data));
   }
 
-  function renderLocations() {
-    return weatherInfo.map((location) => (
-      <li key={location.title} onClick={() => console.log(location.woeid)}>
+  function renderSearch() {
+    return searchInfo.map((location) => (
+      <li key={location.title} onClick={() => fetchWeather(location.woeid)}>
         {location.title}
       </li>
     ));
   }
 
+  function renderWeatherInfo() {
+    if (!weatherInfo.consolidated_weather) {
+      return;
+    } else {
+      return weatherInfo.consolidated_weather.map((day) => (
+        <li key={day.applicable_date}>
+          {day.applicable_date}
+          {day.weather_state_name}
+          {day.the_temp}
+        </li>
+      ));
+    }
+  }
+
   return (
     <Container>
       <Body>
+        <ul>{renderWeatherInfo()}</ul>
         <input
           type="text"
           value={search}
           onChange={(e) => setSearch(e.target.value)}
         />
-        <button onClick={fetchWeather}>Search for Location</button>
-        <ul>{renderLocations()}</ul>
+        <button onClick={fetchSearch}>Search for Location</button>
+        <ul>{renderSearch()}</ul>
       </Body>
     </Container>
   );
@@ -67,5 +90,6 @@ const Body = styled.div`
   & ul {
     list-style: none;
     font-size: 20px;
+    cursor: pointer;
   }
 `;
